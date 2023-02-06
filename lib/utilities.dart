1fe6ab76d382a3e1d007b0a94b76526d04fe60fd
matifpervaizofficial <matifpervaizofficial@gmail.com>
2023-02-06 15:22:45 +0500

    Api setup and json parsing

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,4 +1,4 @@
-// class Api {
-//   static const apiendpoint =
-//       "https://api.themoviedb.org/3/movie/550?api_key=ba7a9eab7a36b323247a68804750d8da";
-// }
+class Api {
+  static const apiendpoint =
+      "https://api.themoviedb.org/3/movie/550?api_key=ba7a9eab7a36b323247a68804750d8da";
+}
